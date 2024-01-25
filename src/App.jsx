5a8a80db2c3878b1734185b6cd9557a4f6216bd1
Rey Viene Dean Pasiculan <88914772+reyviene7@users.noreply.github.com>
2024-01-25 16:21:09 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+function App() {
+  return <div className={styles.App}>
+    <Navbar />
+  </div>
+}
+
+export default App
