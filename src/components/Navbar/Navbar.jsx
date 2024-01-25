de2375a2696740514514dbd9ff707429435291fd
Rey Viene Dean Pasiculan <88914772+reyviene7@users.noreply.github.com>
2024-01-25 17:07:43 +0800

    partial

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,13 +3,25 @@ import styles from './Navbar.module.css';
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
         <a className={styles.title} href="/">
             Portfolio
         </a>
         <div className={styles.menu}>
-            <ul className={styles.menuItems}>
+          <img 
+          className={styles.menuBtn} 
+          src={
+            menuOpen 
+            ? getImageUrl("nav/closeIcon.png") 
+            : getImageUrl("nav/menuIcon.png")
+          } 
+          alt="menu-button" 
+          onClick={() => setMenuOpen(!menuOpen)}
+          />
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#project">Project</a></li>
